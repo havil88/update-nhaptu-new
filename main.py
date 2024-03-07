04773307df475e84dbf70109a3cfc8193919be63
havil88 <wartalemoonhunt1@gmail.com>
2024-03-07 13:52:01 +0700

    First commit on main branch

---
+++ main.py
@@ -0,0 +1,8 @@
+from change import ChuyenDoiChuHoa
+
+def main():
+    # Tạo một thực thể của lớp ChuyenDoiChuHoa
+    chuyen_doi_chu_hoa = ChuyenDoiChuHoa()
+
+if __name__ == "__main__":
+    main()
