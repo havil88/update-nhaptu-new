590dfcb573ab5f8bf9059f711bff759eac5f02c1
havil88 <117423692+havil88@users.noreply.github.com>
2024-05-28 11:47:11 +0700

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from change import ChuyenDoiChuHoa
-
-def main():
-    # Tạo một thực thể của lớp ChuyenDoiChuHoa
-    chuyen_doi_chu_hoa = ChuyenDoiChuHoa()
-
-if __name__ == "__main__":
-    main()
